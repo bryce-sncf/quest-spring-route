0f3789c37c57480d770ff101ee49f39fcd5bfc07
bryce-sncf <bryce.marcol@sncf.fr>
2023-07-17 12:21:32 +0200

    initial commit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,30 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{id}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor getDoctor(@PathVariable int id) {
+        if (id == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (id >= 1 && id < 13) {
+            throw new ResponseStatusException(
+                    HttpStatus.SEE_OTHER,
+                    "Essayez avec l'ID 13 et non l'ID " + id);
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Impossible de récupérer l'incarnation " + id);
+        }
     }
 }
